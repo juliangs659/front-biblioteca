5cdcc74b982c5bd4167c6b102187ce01081b6d46
Andres Garcia <alejogarciamorales@gmail.com>
2024-10-27 11:33:12 -0500

    Pagina home y detalles del libro

---
+++ src/app/single-book/single-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-book.component.html',
+  styleUrl: './single-book.component.css'
+})
+export class SingleBookComponent {
+
+}
